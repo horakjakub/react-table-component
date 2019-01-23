b0aeb538e151aa4e98b789addfd828296df108ef
horakjakub <horak.kuba@gmail.com>
2019-01-23 16:19:15 +0100

    002 - small fixes in init.js file

--- init.js
+++ init.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render }from 'react-dom';
 
-import Table from './src/js/components/Table'
+import Table from './src/js/components/Table';
 
 const appWrapper = document.getElementById('root');
 
