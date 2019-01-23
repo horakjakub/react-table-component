6f3b1edc5eed53bf1ae8766b1b38f63e9e00b3bb
horakjakub <horak.kuba@gmail.com>
2019-01-23 16:28:37 +0100

    002 - removed doubleqoutes from jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    "rootDir": "",
-    "setupTestFrameworkScriptFile": "<rootDir>/test/testSetup.js",
-    "moduleFileExtensions": [
-        "js",
-        "json"
+    'rootDir': '',
+    'setupTestFrameworkScriptFile': '<rootDir>/test/testSetup.js',
+    'moduleFileExtensions': [
+        'js',
+        'json'
     ],
-    "moduleNameMapper": {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/fileMock.js",
-        "\\.(css|scss)$": "<rootDir>/test/styleMock.js"
+    'moduleNameMapper': {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/fileMock.js',
+        '\\.(css|scss)$': '<rootDir>/test/styleMock.js'
     }
 }
